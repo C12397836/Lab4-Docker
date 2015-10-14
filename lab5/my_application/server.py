96645b62a150ef2215aff4c63eeaf0e590dffe88
Paul <paulgood247@gmail.com>
2015-10-14 15:04:05 +0000

    vfsrag

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/')
+def index():
+  return "kjhgvjhfghjdghfjhfhjg"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
+    
+
